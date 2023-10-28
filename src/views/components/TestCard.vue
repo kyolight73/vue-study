075aa09ca38d31df9e74858157585788bf35ecff
DESKTOP-40FHTQ2\ADMIN <immortals730@gmail.com>
2023-10-28 10:27:35 +0700

    initial

---
+++ src/views/components/TestCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card card-test">
+    <img
+      src="https://sgp1.digitaloceanspaces.com/tz-mag-vn/wp-content/uploads/2019/05/020205053737/big-ben-bridge-castle-460672-770x530.jpg"
+      alt="Image placeholder" class="card-img-top" />
+    <div class="row justify-content-center">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test-card",
+};
+</script>
